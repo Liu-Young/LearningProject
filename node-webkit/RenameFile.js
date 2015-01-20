e4a2628d1e706587d08aedf221917f715878d064
Young <as397138316@gmail.com>
2015-01-21 01:57:01 +0800

    test

--- node-webkit/RenameFile.js
+++ node-webkit/RenameFile.js
@@ -1,5 +1,20 @@
 var fs = require('fs');
 
 exports.lowerCase = function (path) {
-    console.log(path);
+    var stat = fs.lstatSync(path);
+    if (stat.isDirectory()) {
+        fs.readdir(path, function (err, files) {
+            if (err) return;
+
+            for (var i = 0; i < files.length; i++) {
+                exports.lowerCase(path + '\\' + files[i]);
+            }
+        });
+    } else {
+        var newPath = path.replace(/JPG/, "jpg");
+        fs.rename(path, newPath, function () {
+            console.log('' + path + '-->' + newPath);
+        });
+    }
+
 }
