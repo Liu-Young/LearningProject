bdb9d8526df2be5c50887972787bf5fe233386a8
chard <as397138316@gmail.com>
2015-02-05 11:13:10 +0800

    init project

--- web/routes/index.js
+++ web/routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+router.get('/article-bonded.html', function(req, res, next) {
+  res.render('article-bonded');
+});
+router.get('/article-info.html', function(req, res, next) {
+  res.render('article-info');
+});
+router.get('/article-logistics.html', function(req, res, next) {
+  res.render('article-logistics');
+});
+router.get('/article-trade.html', function(req, res, next) {
+  res.render('article-trade');
 });
 
 module.exports = router;
